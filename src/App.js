da9f2ede12e71819fb36b6042a6ffb94ab5590b1
Agnieszka Majmurek <majmurek.agnieszka@gmail.com>
2024-07-01 19:04:46 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
+import test from "./test.svg";
 
-function App() {
+function App({id}) {
   return (
     <div className="App">
       <header className="App-header">
